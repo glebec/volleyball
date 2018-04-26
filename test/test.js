0c20ab1b34dadb1c2baccc3319fdbc3205ece25a
Gabriel Lebec <glebec@gmail.com>
2018-04-25 22:22:42 -0400

    fix(all): revert to building all files

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ const express = require('express')
 const stripANSI = require('strip-ansi')
 const debug = require('debug')
 debug.log = console.log.bind(console)
-const volleyball = require('../lib')
+const volleyball = require('../lib/volleyball')
 
 describe('volleyball', function() {
 	it('is middleware', function() {
